99ca54d6b99f6875f000c537fa1399045bc471f7
nishadchittangadan <59539337+nishadchittangadan@users.noreply.github.com>
2021-06-04 23:18:48 +0300

    Initial commit

---
+++ src/com/company/India.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class India {
+    public void speaking(){
+        System.out.println("I can speak indian Language");
+    }
+}
+class Delhi extends India{
+    public void speaking(){
+        System.out.println("I can speak Hindi");
+    }
+}
+class Tamilnadu extends India{
+    public void speaking(){
+        System.out.println("i can speak Tamil");
+    }
+}
+class Kerala extends India{
+    public void speaking(){
+        System.out.println("I can speak Malayalam");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        India l = new India();
+        India l1 = new Delhi();
+        India l2 = new Tamilnadu();
+        India l3 = new Kerala();
+
+        l.speaking();
+        l1.speaking();
+        l2.speaking();
+        l3.speaking();
+    }
+}
